6a626d7dcfd479e4a2dee01443eda2e7cc5f5be5
ianacaburian <ianacaburian@gmail.com>
2018-03-13 22:49:38 +1100

    8 done

--- 2.4_Exercises/Question8.R
+++ 2.4_Exercises/Question8.R
@@ -1,27 +1,46 @@
+rm(list=ls(all=TRUE))
 setwd("C:/GitRepos/Statistical_Learning/2.4_Exercises")
-getwd()
 
 # a)
-college <- read.csv("College.csv")
+college = read.csv("College.csv")
 
 # b)
+# Adjusting data to recognize row names.
 fix(college)
-rownames(college)=college[,1]
+rownames(college) = college[,1]
 fix(college)
-college=college[,-1]
+college = college[,-1]
 fix(college)
 
 # c)
+# Summary and pairs plot.
 summary(college)
 pairs(college)
 
 # Reference the first ten cols of college
-ref10 <- college[,1:10]
+ref10 = college[,1:10]
 pairs(ref10)
 
-attach(college) # Alternatively, access vars via college$Private
-plot(Private, Outstate)
-# NB: Categorical var is passed as x arg for boxplot.
+# Boxplot
+attach(college) 							# saves accessing vars via college$Private
+plot(Private, Outstate)						# plot() will boxplot if its x arg is qualitative
 
+# "Top10perc" is binned to create "Elite" 
+# as the group of unis where more than 
+# 50% of students were from their 
+# high school's top 10%.
+Elite = rep("No", nrow(college))			# replicates values
+Elite[college$Top10perc > 50] = "Yes"
 
+Elite = as.factor(Elite)
+college = data.frame(college, Elite)
+summary(college)
+plot(Elite, Outstate)
 
+# Histograms
+par(mfrow=c(2,2))
+# Percent of faculty with Ph.D.'s
+hist(PhD)
+hist(Grad.Rate)
+hist(Top10perc)
+hist(Top25perc)
